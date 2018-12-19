8cdfcacfbb44261748577c1f6c302ee29c4c770f
ytwu <torrence@mail.ustc.edu.cn>
2018-12-19 10:45:32 +0800

    UPDATE argument config

--- args.py
+++ args.py
@@ -20,18 +20,16 @@ print(sys.argv)
 
 from argparse import ArgumentParser
 
-parser = ArgumentParser()
+parser = ArgumentParser(description='Practice for module argparse')
 # Positional arguments
-parser.add_argument("SAIMOE_YEAR",help="Year of NGA Kancolle Saimoe", type=int, choices=list(range(2014,2019)))
-parser.add_argument("SAIMOE_STAGE",help="Stage of NGA Kancolle Saimoe", choices=['group_stage', 'repechage', '1v1'])
+parser.add_argument("YEAR",help="Year for test", type=int, choices=list(range(2014,2019)))
 # Optional arguments
-parser.add_argument("-g", "--group", help="Group of NGA Kancolle Saimoe : A..Z, 32/16/8/4-A..Z (for 1v1), SA..SZ (for Shinkai)")
 parser.add_argument("--local", help="Do not update thread data, analysis with existed local files", action="store_true", default=False)
 parser.add_argument("-f", "--file", dest="filename",
                   help="write report to FILE", metavar="FILE")
 parser.add_argument("-q", "--quiet",
                   action="store_false", dest="verbose", default=True,
                   help="don't print status messages to stdout")
+parser.add_argument('-i','--info',nargs='+', default=['2018', '1v1', 'A'])
 
-(options, args) = parser.parse_args()
-print(args)
+print(parser.parse_args())
